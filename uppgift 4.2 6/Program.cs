4d473a7217f456f823382df2207ce904df2f691f
Mopa01 <mopa05001@utb.vaxjo.se>
2022-10-21 10:40:37 +0200

    Add project files.

---
+++ uppgift 4.2 6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace uppgift_4._26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.WriteLine("skriv ett heltal");
+                string heltal = Console.ReadLine();
+
+                for (int i = 0; i < heltal.Length; i++)
+                {
+                    string tecken = heltal[i].ToString();
+                    int siffra = int.Parse(tecken);
+
+                    switch (siffra)
+                    {
+                        case 9:
+                            siffra = 0;
+                            break;
+                        default:
+                            siffra++;
+                            break;
+                    }
+                    Console.WriteLine(siffra);
+
+                }
+
+
+            }
+        }
+    }
+}
